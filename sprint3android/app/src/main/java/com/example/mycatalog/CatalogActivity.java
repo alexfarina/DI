40059bbf2ab7a88d5350ce128e892c60aec07b5a
Alex Fariña <afarinac23@fpcoruna.afundacion.org>
2025-11-04 12:20:33 +0100

    ej2 sprint3android

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,18 +1,27 @@
 package com.example.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class CatalogActivity extends AppCompatActivity {
-
+Button button;
+Context context=this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_catalog);
+        button=findViewById(R.id.btnDetail);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myintent=new Intent(context,DetailActivity.class);
+                context.startActivity(myintent);
+            }
+        });
     }
 }
